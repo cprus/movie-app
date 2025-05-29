8b2f71115f3b8298ad88a73a83b99e687eda16ac
Collin P <collinprusse@mac.com>
2025-05-28 20:44:24 -0600

    bottom nav bar and screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,19 @@ import { Stack } from "expo-router";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      {/* <StatusBar hidden={true} /> */}
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="movie/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
+  )
 }
