2897e917e6985dbcd082a9799ecafb0e27ee5079
Collin P <collinprusse@mac.com>
2025-07-23 20:13:38 -0600

    search, trending and details complete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
   return (
     <>
-      {/* <StatusBar hidden={true} /> */}
-      <Stack>
+      <StatusBar hidden={true} />
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen 
           name="(tabs)"
           options={{ headerShown: false }}
